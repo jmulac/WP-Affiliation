07d8431ab0d240620b905cabe28f5d0400c0ac58
Julien <julien.mulac@gmail.com>
2017-07-17 17:10:32 +0200

    amazon fix

--- classes/AffiliateUrl.php
+++ classes/AffiliateUrl.php
@@ -57,14 +57,13 @@ var_dump($domain);
 				$new_url = "https://ad.zanox.com/ppc/?36429178C54756398&ulp=[[".urlencode($clean_url)."?refer=zanoxpb&cid=affil&cm_mmc=zanoxpb-_-userid]]";
 				break;
 			case 'www.amazon.fr': case 'amazon.fr':
-				// Link ID ?
-				$base_url = "https://www.amazon.fr//ref=as_li_ss_tl?ie=UTF8&linkCode=ll2&tag=choc0e-21&linkId=e975064ea5a3cefd29ccded6f0363778";
+				$new_url = \affiliatelinkupd\UrlUtils::add_params_to_url($clean_url, array('tag' => 'choc0e-21'));
 				break;
 			case 'www.instant-gaming.com': case 'instant-gaming.com':
 			// http://stackoverflow.com/questions/4937478/strip-off-url-parameter-with-php
 				$new_url = \affiliatelinkupd\UrlUtils::add_params_to_url($clean_url, array('igr' => 'gamer-abaaf0'));
 			
-				break,
+				break;
 			default:
 				$this->state = self::$_ERROR_STATE;
 				$new_url = $url;
